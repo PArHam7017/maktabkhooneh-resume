cc5ef92f64e5ae630a2331eecc6dfd9d0ca55797
PArHam <prophet7017@gmail.com>
2019-10-27 22:23:32 +0330

    Updates

---
+++ precache-manifest.6a1d388ce0de487eea28776660194d98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff2560b46b132b58fad1e40a4e41139",
+    "url": "/PArHam7017//maktabkhooneh-resume/index.html"
+  },
+  {
+    "revision": "b44083cb45a55d710c93",
+    "url": "/PArHam7017//maktabkhooneh-resume/static/css/main.5285e64d.chunk.css"
+  },
+  {
+    "revision": "fe8407e9492e86450528",
+    "url": "/PArHam7017//maktabkhooneh-resume/static/js/2.c85591a2.chunk.js"
+  },
+  {
+    "revision": "b44083cb45a55d710c93",
+    "url": "/PArHam7017//maktabkhooneh-resume/static/js/main.38f20261.chunk.js"
+  },
+  {
+    "revision": "32319a420787979d3349",
+    "url": "/PArHam7017//maktabkhooneh-resume/static/js/runtime-main.cdf98f59.js"
+  }
+]);
